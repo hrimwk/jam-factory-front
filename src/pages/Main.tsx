2ae426b6b01bedf199fb620dfb6cd6fc41ae5a62
hrim <hrim.wk@gmail.com>
2022-11-28 14:12:56 +0900

    [Delete] 불필요한 import 파일

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,13 +1,5 @@
 import Sliders from '../components/Sliders';
 import styled from 'styled-components';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-
-import slide_1 from '../assets/images/slide_1.jpg';
-import slide_2 from '../assets/images/slide_2.jpg';
-import slide_3 from '../assets/images/slide_3.jpg';
-import slide_4 from '../assets/images/slide_4.jpg';
-import slide_5 from '../assets/images/slide_5.jpg';
 
 export interface sliderSetting {
   dots: boolean;
@@ -30,7 +22,6 @@ function Main() {
     // autoplay: true,
     // autoplaySpeed: 3000,
   };
-  const imgList = [slide_1, slide_2, slide_3, slide_4, slide_5];
 
   return (
     <MainContainer>
