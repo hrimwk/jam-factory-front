092143e6a903f9050fbd38f63e2210d86b6e8624
hrim <hrim.wk@gmail.com>
2023-01-03 16:41:49 +0900

    [Add] signup form UI (주소 api x)

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
 import GlobalStyles from './assets/styles/GlobalStyles';
 import theme from './assets/styles/theme';
 import { ThemeProvider } from 'styled-components';
+
 import Main from './pages/Main';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -10,6 +10,7 @@ import Products from './pages/Products';
 import SignUp from './pages/SignUp';
 import Notice from './pages/Notice';
 import Events from './pages/Events';
+import SignUpForm from './pages/SignUpForm';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Main />}></Route>
           <Route path="/signup" element={<SignUp />}></Route>
+          <Route path="/signup/form" element={<SignUpForm />}></Route>
           <Route path="/products" element={<Products />}></Route>
           <Route path="/notice" element={<Notice />}></Route>
           <Route path="/events" element={<Events />}></Route>
---
+++ src/pages/SignUpForm.tsx
@@ -0,0 +1,134 @@
+import styled from 'styled-components';
+
+import StepIcon from '../components/signup/StepIcon';
+
+function SignUpForm() {
+  const NUMBER_OPTIONS = [
+    { id: 1, num: '010' },
+    { id: 2, num: '011' },
+    { id: 3, num: '016' },
+    { id: 4, num: '017' },
+    { id: 5, num: '018' },
+    { id: 6, num: '019' },
+    { id: 7, num: '02' },
+  ];
+  return (
+    <SignUpFormContainer className="container">
+      <StepIcon />
+      <form>
+        <fieldset>
+          <legend>
+            정보 입력 <span className="cr_red"> &nbsp;&nbsp; * 표시는 필수 항목입니다.</span>
+          </legend>
+          <ul className="join_box">
+            <li className="form-element">
+              <label>
+                아이디 <span className="cr_red">*</span>
+              </label>
+              <div>
+                <input type="text" className="mr-10" />
+                <button className="button">중복확인</button>
+              </div>
+            </li>
+            <li className="form-element">
+              <label>
+                비밀번호 <span className="cr_red">*</span>
+              </label>
+              <input type="password" />
+            </li>
+            <li className="form-element">
+              <label>
+                비밀번호 확인 <span className="cr_red">*</span>
+              </label>
+              <input type="text" />
+            </li>
+            <li className="form-element">
+              <label>주소</label>
+            </li>
+            <li className="form-element">
+              <label>
+                휴대폰 <span className="cr_red">*</span>
+              </label>
+              <div className="phone-num-area">
+                <select className="mr-10">
+                  {NUMBER_OPTIONS.map((data) => {
+                    return <option key={data.id}>{data.num}</option>;
+                  })}
+                </select>
+                <input type="text" className="mr-10" />
+                <input type="text" />
+              </div>
+            </li>
+            <li className="form-element">
+              <label>이메일</label>
+              <div>
+                <input type="text" /> @ <input type="text" />
+              </div>
+            </li>
+          </ul>
+        </fieldset>
+        <div className="button-area">
+          <button className="button">회원가입</button>
+        </div>
+      </form>
+    </SignUpFormContainer>
+  );
+}
+const SignUpFormContainer = styled.div`
+  .mr-10 {
+    margin-right: 10px;
+  }
+  form {
+    width: 40%;
+    margin: 50px auto;
+  }
+  fieldset {
+    border: none;
+  }
+  label {
+    font-size: 14px;
+  }
+  input,
+  select {
+    padding: 10px;
+    border-radius: 5px;
+    border: 1px solid #ddd;
+  }
+  legend {
+    padding-bottom: 30px;
+    font-size: 20px;
+    font-weight: 600;
+    color: rgb(153, 150, 144);
+  }
+  .form-element {
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+    column-gap: 20px;
+    margin-bottom: 10px;
+  }
+  .phone-num-area {
+    input {
+      width: 33%;
+    }
+  }
+  .button {
+    padding: 10px;
+    border: none;
+    background: rgb(123, 95, 60);
+    border-radius: 5px;
+    text-align: center;
+    color: #fff;
+    width: 80px;
+  }
+  .cr_red {
+    font-size: 10px;
+    color: rgb(190, 20, 30);
+  }
+
+  .button-area {
+    display: flex;
+    justify-content: center;
+    margin-top: 30px;
+  }
+`;
+export default SignUpForm;
