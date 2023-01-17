78470f4da413905df3fe53adfba63da3c1a815a0
hrim <hrim.wk@gmail.com>
2023-01-17 12:41:52 +0900

    [Add] footer

---
+++ src/assets/utils/main/footerNav.ts
@@ -0,0 +1,28 @@
+const FOOTER_NAV = [
+  {
+    id: 1,
+    link: '/',
+    name: '홈',
+  },
+  {
+    id: 2,
+    link: '/',
+    name: '회사소개',
+  },
+  {
+    id: 3,
+    link: '/',
+    name: '이용약관',
+  },
+  {
+    id: 4,
+    link: '/',
+    name: '개인정보처리방침',
+  },
+  {
+    id: 5,
+    link: '/',
+    name: '이용안내',
+  },
+];
+export { FOOTER_NAV };
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,48 @@
-function Footer() {
-  return <>footer</>;
-}
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
+import logo_1 from '../assets/images/logo_1.png';
+import { FOOTER_NAV } from '../assets/utils/main/footerNav';
+
+function Footer() {
+  return (
+    <FooterContainer>
+      <div className="footer-logo">
+        <img src={logo_1} />
+      </div>
+      {FOOTER_NAV && (
+        <ul className="footer-menu">
+          {FOOTER_NAV.map((data) => {
+            return (
+              <li key={data.id}>
+                <Link to={data.link}>{data.name}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </FooterContainer>
+  );
+}
+const FooterContainer = styled.footer`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 20px;
+  width: 100%;
+  padding: 50px 0;
+  background: ${({ theme }) => theme.colors.subIvory};
+  .footer-logo {
+    width: 170px;
+  }
+  .footer-menu {
+    display: flex;
+    justify-content: space-between;
+    width: 350px;
+  }
+  .footer-menu li a {
+    font-size: 12px;
+    color: ${({ theme }) => theme.colors.subBrown};
+  }
+`;
 export default Footer;
