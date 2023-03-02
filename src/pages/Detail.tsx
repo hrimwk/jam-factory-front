b4cad79276deafd92aff683c6a073a84f698452f
hrim <hrim.wk@gmail.com>
2023-03-02 12:23:04 +0900

    [Fix] conflict

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -6,14 +6,7 @@ import productImg from '../assets/images/jam_12.jpeg';
 function Detail() {
   const [amount, setAmount] = useState(0);
   const [index, setTabIndex] = useState('1');
-  const [index, setTabIndex] = useState('1');
   const price = 3880;
-  const TAB_LIST = [
-    { id: '1', name: '상품 상세', content: '' },
-    { id: '2', name: '상품 후기', content: '' },
-    { id: '3', name: '문의', content: '' },
-    { id: '4', name: '배송/교환/반품', content: '' },
-  ];
   const TAB_LIST = [
     { id: '1', name: '상품 상세', content: '' },
     { id: '2', name: '상품 후기', content: '' },
@@ -26,24 +19,18 @@ function Detail() {
   const clickTab = (e: React.MouseEvent) => {
     setTabIndex(e.currentTarget.id);
   };
-  const clickTab = (e: React.MouseEvent) => {
-    setTabIndex(e.currentTarget.id);
-  };
   return (
     <DetailContainer className="container">
       <div className="wrap">
         <div className="product-img">
           <img src={productImg} alt="" />
         </div>
-
         <div className="product-info">
           <p className="title">[복음자리] 딸기잼 230g </p>
           <p className="price">3,880원</p>
           <p className="delivery-fee">배송비 3,000원 (30,000원 이상 구매시 무료)</p>
           <div className="amount">
             <label>수량</label>
-            <input type="number" min="0" max="10" defaultValue="0" className="number" onChange={changeAmount} />
-            <span>최대 구매 수량 10개</span>
             <input type="number" min="0" max="10" defaultValue="0" className="number" onChange={changeAmount} />
             <span>최대 구매 수량 10개</span>
           </div>
@@ -70,30 +57,15 @@ function Detail() {
         </ul>
         <div className="content"></div>
       </section>
-      <section className="tab">
-        <ul>
-          {TAB_LIST.map((data) => {
-            return (
-              <li id={data.id} key={data.id} className={index === data.id ? 'active' : undefined} onClick={clickTab}>
-                {data.name}
-              </li>
-            );
-          })}
-        </ul>
-        <div className="content"></div>
-      </section>
     </DetailContainer>
   );
 }
 const DetailContainer = styled.div`
   width: 60%;
   margin: 0 auto;
-  width: 60%;
-  margin: 0 auto;
   .wrap {
     display: flex;
     justify-content: space-between;
-    margin-bottom: 80px;
     margin-bottom: 80px;
   }
   .product-img {
@@ -120,11 +92,6 @@ const DetailContainer = styled.div`
     label {
       display: block;
       margin-bottom: 10px;
-    }
-    span {
-      margin-left: 10px;
-      font-size: 12px;
-      color: #999;
     }
     span {
       margin-left: 10px;
@@ -175,30 +142,6 @@ const DetailContainer = styled.div`
       color: #fff;
       background: ${({ theme }) => theme.colors.mainBrown};
     }
-  }
-  .tab {
-    overflow: hidden;
-    ul {
-      display: flex;
-    }
-    li {
-      flex: 1 1 25%;
-      padding: 15px 10px;
-      text-align: center;
-      background: ${({ theme }) => theme.colors.lightGray};
-      cursor: pointer;
-    }
-    .content {
-      min-height: 500px;
-      border: 1px solid ${({ theme }) => theme.colors.lightGray};
-      border-top: 0px;
-    }
-  }
-  .active {
-    font-weight: bold;
-    border: 1px solid ${({ theme }) => theme.colors.lightGray};
-    border-bottom: 0px;
-    background: #fff !important;
   }
   .tab {
     overflow: hidden;
